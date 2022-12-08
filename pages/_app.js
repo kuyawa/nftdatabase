879966f019fc05c064a77cfdf9f551dcc3118dcc
Kuyawa <kuyawakata@gmail.com>
2022-12-08 10:47:58 -0400

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-import { SessionProvider } from "next-auth/react";
+import "/styles/globals.css";
 
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp;
