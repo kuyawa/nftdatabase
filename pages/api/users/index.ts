89cf456a28c99afac52bd0537ff05946a8c875f8
Kuyawa <kuyawakata@gmail.com>
2022-12-08 15:59:55 -0400

    Home page

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -42,6 +42,27 @@ export default async function handler(req, res) {
         return res.status(400).json({ success: false, error: error.message })
       }
       break
+    case "PUT":
+      try {
+        let authorized = await checkApiKey(req.headers['x-api-key'])
+        if (!authorized) {
+          return res.status(403).json({ success: false, error: 'Not authorized' })
+        }
+        let data = req.body
+        let {id, ...newData} = data
+        const prisma = new PrismaClient()
+        await prisma.$connect()
+        let result = await prisma.users.update({
+          where: {id: id},
+          data: newData
+        })
+        await prisma.$disconnect();
+        return res.status(201).json({ success: true, data: result })
+      } catch (error) {
+        console.error('REGISTRY ERROR', { error })
+        return res.status(400).json({ success: false, error: error.message })
+      }
+      break
     default:
       res.status(400).json({ success: false, error:'HTTP method not supported' });
       break;
