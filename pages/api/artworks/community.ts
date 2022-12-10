c45c672f29aaedf523c46cc0e9eef484ae821d01
Kuyawa <kuyawakata@gmail.com>
2022-12-09 21:03:20 -0400

    Curated

---
+++ pages/api/artworks/community.ts
@@ -0,0 +1,68 @@
+import { PrismaClient } from "@prisma/client"
+// @ts-ignore
+import checkApiKey from "/lib/checkApiKey"
+
+export default async function handler(req, res) {
+  let { method, headers, query } = req
+  console.log('- ARTWORKS COMMUNITY', method)
+  switch (method) {
+    // GET /api/artworks/curated[?page=0&size=100]
+    // gets latest curated artworks
+    // page and size are optional
+    case "GET":
+      try {
+        let authorized = await checkApiKey(headers['x-api-key'])
+        if (!authorized) {
+          return res.status(403).json({ success: false, error:'Not authorized' })
+        }
+        let page = parseInt(query?.page || 0)
+        let size = parseInt(query?.size || 100)
+        if(page<0){ page = 0 }
+        if(size<0){ size = 100 }
+        let start = page * size
+        console.log('START', start, 'PAGE', page, 'SIZE', size)
+        let prisma = new PrismaClient()
+        await prisma.$connect()
+        let data = await prisma.artworks.findMany({ 
+          include: { author: true },
+          where: {
+            collection: { 
+               curated: false
+            }
+          },
+          skip: start, 
+          take: size, 
+          orderBy: {created: 'desc'} 
+        })
+        await prisma.$disconnect()
+        console.log(data?.length||0, 'rows')
+        res.status(200).json({ success: true, data: data })
+      } catch (error) {
+        console.log({ error })
+        res.status(400).json({ success: false, error:error.message })
+      }
+      break
+    // POST /api/artworks {form:data}
+    // create new nft artwork
+    case "POST":
+      try {
+        let authorized = await checkApiKey(req.headers['x-api-key'])
+        if (!authorized) {
+          return res.status(403).json({ success: false, error: 'Not authorized' })
+        }
+        let data = req.body
+        let prisma = new PrismaClient()
+        await prisma.$connect()
+        let result = await prisma.artworks.create({data: data})
+        await prisma.$disconnect()
+        return res.status(201).json({ success: true, data: result })
+      } catch (error) {
+        console.error('REGISTRY ERROR', { error })
+        return res.status(400).json({ success: false, error: error.message })
+      }
+      break
+    default:
+      res.status(400).json({ success: false, error:'HTTP method not accepted' })
+      break
+  }
+}
